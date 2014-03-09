3183aa242465d1e1e945414d646c81babff5ecbf
Gavin Thompson <gavinathompson@gmail.com>
2014-03-09 17:43:43 -0400

    Divided layout into header/layout; added signup/login buttons/login to dropdown (created header to accept email only)

---
+++ client/helpers/config.js
@@ -0,0 +1,5 @@
+Accounts.ui.config({ 
+
+	passwordSignupFields: 'EMAIL_ONLY'
+
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -3,19 +3,22 @@ if (Ipsums.find().count() === 0) {
 	Ipsums.insert({
 		title: 'He-Man Ipsum',
 		author: 'Gavin T',
-		uniqueIdentifier: "eternia",
+		subdomain: "eternia",
+		description: "A small description about this Ipsum",
 		wordList: ['He-Man', 'Skeletor', 'Prince Adam', 'Eternia', 'Battlecat' ]
 	});
 	Ipsums.insert({
 		title: 'Pokem Ipsum',
 		author: 'Gavin T',
-		uniqueIdentifier: "pokemipsum",
+		subdomain: "pokemipsum",
+		description: "A small description about this Ipsum",
 		wordList: ['Squirtle', 'Bulbasaur', 'Charizard', 'Blastoise', "Pokemon", "Brock", "Misty" ]
 	});
 	Ipsums.insert({
 		title: 'Fordem Ipsum',
 		author: 'Gavin T',
-		uniqueIdentifier: "fordnation",
+		subdomain: "fordnation",
+		description: "A small description about this Ipsum",
 		wordList: ['Subways subways subways', 'tax payers', 'subways', 'orientals', 'gravy', 'gravytrain', 'Steak Queen', 'Rob Ford', "Ford Nation" ]
 	}); 
 	
