c7f7604555b169966ad1eeae5a32d9c9345b6ea6
Gavin Thompson <gavinathompson@gmail.com>
2014-03-09 19:15:11 -0400

    Adding additional logic to ipsum_submit.js to turn wordList's raw comma-delimited string into array

--- client/views/ipsums/ipsum_submit.js
+++ client/views/ipsums/ipsum_submit.js
@@ -1,12 +1,27 @@
 Template.ipsumSubmit.events({ 
 		'submit form': function(e) {
 	    e.preventDefault();
+
+	    var wordListRawData = $(e.target).find('[name=wordList]').val()
+	    var wordListArray = []
+	    turnWordListIntoArray()
+
+
+	    function turnWordListIntoArray(){
+
+	    	alert(wordListRawData)
+	    	wordListArray = wordListRawData.split(",")
+	    	alert(wordListArray)
+
+	    }
+
 		
 		var ipsum = {
 			subdomain: $(e.target).find('[name=subdomain]').val(),
 			title: $(e.target).find('[name=title]').val(), 
 			description: $(e.target).find('[name=description]').val(),
-			wordList: $(e.target).find('[name=wordList]').val()
+			// wordList: $(e.target).find('[name=wordList]').val()
+			wordList: wordListArray
 		}
 
 
