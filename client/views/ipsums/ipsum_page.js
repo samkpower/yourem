3a7eb682fd98e8a0c2463e12a23e266f734e1284
Gavin Thompson <gavinathompson@gmail.com>
2014-03-13 23:45:19 -0400

    Expanding paragraph generation functionality in ipsum_page.js

--- client/views/ipsums/ipsum_page.js
+++ client/views/ipsums/ipsum_page.js
@@ -1,3 +1,11 @@
+var currentIpsum = null
+var currentIpsumArray = null
+var arrayLength = null
+var numberParagraphs = null
+var canvasHeight = null
+var paragraphStyle = null
+
+
 Template.ipsumPage.rendered = function() {
     // if(!this._rendered) {
     //   this._rendered = true;
@@ -6,73 +14,167 @@ Template.ipsumPage.rendered = function() {
 
     $('#ipsum-canvas').css('opacity', 0);
     $('#ipsum-canvas h1').css('opacity', 0);
+
+
+    currentIpsum = this.data
+    currentIpsumArray = this.data.wordList
+    arrayLength = currentIpsumArray.length
+    // console.log(currentIpsumArray)
 };
 
 
-Template.ipsumPage.events({ 
 
+Template.ipsumPage.events({ 
 
 	'submit form': function(e) {
 		e.preventDefault();
 
 		// FADE OUT canvas/canvas content
-						$('#ipsum-canvas').css('opacity', 0);
-					    $('#ipsum-canvas h1').css('opacity', 0);
+				$('#ipsum-canvas').css('opacity', 0);
+			    $('#ipsum-canvas h1').css('opacity', 0);
 
 	    // Find and set paragraph variables
-						var paragraphs = $(e.target).find('[name=paragraphs]').val()
-						var canvasHeight = (paragraphs * 90) +"px"
+				numberParagraphs = $(e.target).find('[name=paragraphs]').val()
+				canvasHeight = (numberParagraphs * 130) +"px" //NEEDS TO BE DYNAMIC
 
+		// Find/Set style of paragraphs 
+				paragraphStyle = $(e.target).find('[name=contentStyle]').val()
 
 		// Animate canvas and shit in
-						$('#ipsum-canvas').animate({
-							opacity: 1, 
-							height: canvasHeight
-						}, 1200, function() {
-					        // Animation complete.
-					    });
+				animateCanvas()
 
-						$('#ipsum-canvas h1').animate({
-							opacity: 1, 
-						}, 500, function() {
-					        // Animation complete.
-					    });
+				paragraphGeneration()
 
 
-						// 'submit form': function(e) {
-						// 	e.preventDefault();
+		// Set functions
 
-						// 	// FADE OUT canvas/canvas content
-						// 					$('#ipsum-canvas').css('opacity', 0);
-						// 				    $('#ipsum-canvas h1').css('opacity', 0);
+				function animateCanvas(){
+					$('#ipsum-canvas').animate({
+						opacity: 1, 
+						height: canvasHeight
+					}, 1200, function() {
+				        // Animation complete.
+				    });
 
-						// 				    console.log("dkfnkdsfnsdnkjdsfnkj");
-						// 	// Animate canvas and shit in
-						// 					generatedIpsumData.revealCanvas();
+					$('#ipsum-canvas h1').animate({
+						opacity: 1, 
+					}, 500, function() {
+				        // Animation complete.
+				    });
+				};
 
 
-						// 		var generatedIpsumData = {
+				function generateParagraph(){
+					// 
+					// 
+					// Set up sentence scoped variables
+					var completedParagraph = null
+					var newParagraph = []
 
-						// 				// Find and set paragraph variables
-						// 					paragraphs: $(e.target).find('[name=paragraphs]').val(),
-						// 					canvasHeight: (generatedIpsumData.paragraphs * 90) +"px",
+					var paragraphSentenceLength = Math.floor(( Math.random() * (10-5) )+5);
 
-						// 				revealCanvas: function(){
-						// 					$('#ipsum-canvas').animate({
-						// 						opacity: 1, 
-						// 						height: generatedIpsumData.canvasHeight
-						// 					}, 1200, function() {
-						// 				        // Animation complete.
-						// 				    });
 
-						// 					$('#ipsum-canvas h1').animate({
-						// 						opacity: 1, 
-						// 					}, 500, function() {
-						// 				        // Animation complete.
-						// 				    });
-						// 				}
-						// 		}	
-						// }
+					// Loop paragraphlength and generate a sentence for each
+					for (var i = 0; i < paragraphSentenceLength; i++) {
+
+							var paragraphSentence = generateSentence()
+							console.log(paragraphSentence)
+							newParagraph.push( paragraphSentence )
+							console.log(newParagraph)
+
+
+					};
+
+					console.log("end loop")
+					console.log(newParagraph)
+
+					completedParagraph = _stringifyNewParagraph(newParagraph)
+					console.log(completedParagraph)
+
+					// FINISH FUNCTION
+					return completedParagraph
+
+					// SUPPORTING FUNCTION 
+						function _stringifyNewParagraph(array){
+							newParagraphString = array.join(' ')
+							return newParagraphString
+						};
+					// END Supporting Functions
+				}; // end
+
+
+
+
+				function paragraphGeneration(){
+					console.log( generateParagraph() )
+					$("#ipsum-canvas").empty()
+
+					for (var i = 0; i < numberParagraphs; i++) {
+							var finishedParagraph = generateParagraph()
+							$("#ipsum-canvas").append("<div><p>"+ finishedParagraph+"</p></div>")
+					};
+				};  // end
+
+
+
+
+				function generateSentence(){
+					// 
+					// Set up sentence scoped variables
+					var completedSentence = null
+					var newSentence = []
+					
+					// get number of words in each sentence randomally
+					var sentenceWordLength = Math.floor(( Math.random() * (19-9) )+9);
+
+					// run get random word function ...
+					// run this function X number of times where X = sentenceWordLength
+
+					for (var i = 0; i < sentenceWordLength; i++) {
+							var newWord = _getRandomWord()
+							newSentence.push( newWord )
+					};
+
+					console.log(newSentence);
+					completedSentence = _stringifySentence(newSentence);
+					console.log(completedSentence);
+
+					// FINISH FUNCTION
+					return completedSentence
+							// if latin selected do something else.....
+
+
+					// 
+					// Supporting FUNCTIONS
+						function _getRandomWord(){
+							// 
+							// get random number between 0 and wordList.length
+							var randomWordValue = Math.floor((Math.random() * arrayLength)+0);
+
+							// turn randomWordValue into actual word
+							randomWord = currentIpsumArray[randomWordValue]
+							// return word
+							return randomWord
+						};
+
+						function _stringifySentence(array){
+							var newString = null;
+							newString = array.join(' ');
+							newString = newString.toLowerCase();
+							newString = _capitalizeFirstLetter(newString);
+							newString = newString + ".";
+
+							return newString
+
+						};
+
+						function _capitalizeFirstLetter(string){
+						    return string.charAt(0).toUpperCase() + string.slice(1);
+						};
+					// END Supporting Functions
+				}; // end
+				
 		
+
 	}
 });
