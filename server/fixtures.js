3e7a38643b07332afbd86180e455f36b6ea7addc
Gavin Thompson <gavinathompson@gmail.com>
2014-06-15 18:09:21 -0400

    Adding add'tnl demo/default ipsum

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,8 +3,15 @@ if (Ipsums.find().count() === 0) {
 	Ipsums.insert({
 		title: 'Chickem Parmsum',
 		author: 'Gavin T',
-		description: "A small description about Chicken Parmesean",
+		description: "An Ipsum for every Chicken Parmesean occassion.",
 		wordList: ['Chicken', 'Parmesean', 'Mozzarella', 'Tomato Sauce', 'Wine', "Noodles", "Chicken thighs", "chicken breasts", "oozy", "delicious", "breadcrumbs", "saucy" ]
+	});
+
+	Ipsums.insert({
+		title: 'Poke Ipsum',
+		author: 'Gavin T',
+		description: "Gotta Catch 'em all! Demo Pokemon Ipsum with 80+ pokemon",
+		wordList: ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidoran", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing" ]
 	});
 	// Ipsums.insert({
 	// 	title: 'Pokem Ipsum',
