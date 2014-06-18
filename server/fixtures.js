9e618d4af79868469b58c1d222588372505e8a52
Gavin Thompson <gavinathompson@gmail.com>
2014-06-17 21:24:29 -0400

    Adding submitted data to fixtures; also reimplementing sorting helper on ipsums list to sort by most recent ipsum

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,6 +4,7 @@ if (Ipsums.find().count() === 0) {
 		title: 'Chickem Parmsum',
 		author: 'Gavin T',
 		description: "An Ipsum for every Chicken Parmesean occassion.",
+		submitted: 1403053446565,
 		wordList: ['Chicken', 'Parmesean', 'Mozzarella', 'Tomato Sauce', 'Wine', "Noodles", "Chicken thighs", "chicken breasts", "oozy", "delicious", "breadcrumbs", "saucy" ]
 	});
 
@@ -11,6 +12,7 @@ if (Ipsums.find().count() === 0) {
 		title: 'Poke Ipsum',
 		author: 'Gavin T',
 		description: "Gotta Catch 'em all! Demo Pokemon Ipsum with 80+ pokemon",
+		submitted: 1403053446564,
 		wordList: ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidoran", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing" ]
 	});
 	// Ipsums.insert({
